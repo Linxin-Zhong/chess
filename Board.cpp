7fae7cfba8fc7148cda8cfcf9db5889f276ecc25
Kkathyf <95158976+Kkathyf@users.noreply.github.com>
2021-11-29 12:02:36 +0800

    Update Board.cpp
    uppercase

--- Board.cpp
+++ Board.cpp
@@ -10,7 +10,7 @@ bool Board::isCheckMate() {
 
 }
 
-bool Board::isStakemate() {
+bool Board::isStalemate() {
 
 }
 
@@ -25,23 +25,23 @@ bool Board::isRun() {
 Board::Board(char w, char b) {
     switch (w) {
         case 'h':
-            this->WPlayer = make_shared<human>(human(make_shared<Board>(*this)));
+            this->WPlayer = make_shared<Human>(Human(make_shared<Board>(*this)));
         case '1':
-            this->WPlayer = make_shared<level1>(level1(make_shared<Board>(*this)));
+            this->WPlayer = make_shared<Level1>(Level1(make_shared<Board>(*this)));
         case '2':
-            this->WPlayer = make_shared<level2>(level1(make_shared<Board>(*this)));
+            this->WPlayer = make_shared<Level2>(Level1(make_shared<Board>(*this)));
         case '3':
-            this->WPlayer = make_shared<level2>(level1(make_shared<Board>(*this)));
+            this->WPlayer = make_shared<Level2>(Level1(make_shared<Board>(*this)));
     }
     switch (b) {
         case 'h':
-            this->BPlayer = make_shared<human>(human(make_shared<Board>(*this)));
+            this->BPlayer = make_shared<Human>(Human(make_shared<Board>(*this)));
         case '1':
-            this->BPlayer = make_shared<level1>(level1(make_shared<Board>(*this)));
+            this->BPlayer = make_shared<Level1>(Level1(make_shared<Board>(*this)));
         case '2':
-            this->BPlayer = make_shared<level2>(level1(make_shared<Board>(*this)));
+            this->BPlayer = make_shared<Level2>(Level1(make_shared<Board>(*this)));
         case '3':
-            this->BPlayer = make_shared<level2>(level1(make_shared<Board>(*this)));
+            this->BPlayer = make_shared<Level2>(Level1(make_shared<Board>(*this)));
     }
 };
 
