eaef869581cd8149bf404664c974de84cb1b834c
CatttttttV <95162004+CatttttttV@users.noreply.github.com>
2021-12-07 18:28:12 +0800

    Update pawn.h

--- pawn.h
+++ pawn.h
@@ -8,6 +8,10 @@ public:
     Pawn(char color, std::vector<std::vector<std::shared_ptr<Piece>>> &boardmap);
 
     bool check(int torow, int tocol, int kingrow, int kingcol);
+    pair<char, int> legalMove(int r, int c);
+    pair<char, int> capture(int r, int c);
+    pair<char, int> avoid(int r, int c);
+    pair<char, int> checks(int r, int c);
 };
 
 #endif
