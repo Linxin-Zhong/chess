351d54c014b7c145c100e66c93fb3d3850e99a69
CatttttttV <95162004+CatttttttV@users.noreply.github.com>
2021-12-07 16:16:32 +0800

    Update rook.h

--- rook.h
+++ rook.h
@@ -9,6 +9,10 @@ public:
     Rook(char color, std::vector<std::vector<std::shared_ptr<Piece>>> &boardmap);
 
     bool check(int torow, int tocol, int kingrow, int kingcol);
+    vector<pair<char, int>> legalMove();
+    vector<pair<char, int>> capture();
+    vector<pair<char, int>> avoid();
+    vector<pair<char, int>> checks();
 };
 
 #endif
