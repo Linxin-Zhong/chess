a155d05d1f70564d986b868f1446a7673eb5d707
CatttttttV <95162004+CatttttttV@users.noreply.github.com>
2021-12-10 03:38:42 +0800

    Update knight.cpp

--- knight.cpp
+++ knight.cpp
@@ -64,8 +64,43 @@ vector<pair<int, int>> Knight::captureMoves(int r, int c) {
 }
 
 vector<pair<int, int>> Knight::avoidMoves(int r, int c) {
-    vector<pair<int, int>> a;
-    return a;
+    
+    // check all anticolor positions
+    vector<pair<int, int>> positions;
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            if ((*boardmap)[i][j]->getColor() != this->color) {
+                pair<int, int> temp = {i, j};
+                positions.emplace_back(temp);
+            }
+        }
+    }
+
+    vector<pair<int, int>> listOfAvoidMoves;
+    vector<pair<int, int>> dir = {{1, 2},
+                                  {1, -2},
+                                  {-1, 2},
+                                  {-1, -2},
+                                  {2, 1},
+                                  {2, -1},
+                                  {-2, 1},
+                                  {-2, -1}};                      
+    for (int i = 0; i < 8; i++) {
+        int newrow = r + dir[i].first;
+        int newcol = c + dir[i].second;
+        if (newrow < 0 || newrow >= 8 || newcol < 0 || newcol >= 8) {
+            continue;
+        }
+        if (!(*boardmap)[newrow][newcol]) {
+            for (int j = 0; j < positions.size(); j++) {
+                if (!(*boardmap)[newrow][newcol]->isCheck(*boardmap, this->color, newrow, newcol, &positions[j].first, &positions[j].second)) {
+                    pair<int, int> temp = {newrow, newcol};
+                    listOfAvoidMoves.emplace_back(temp);
+                }
+            }   
+        }
+    }
+    return listOfAvoidMoves;
 }
 
 vector<pair<int, int>> Knight::checkMoves(int r, int c) {
